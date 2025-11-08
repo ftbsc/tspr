7060605d89ceb7dd06ad5b972c690ad039105c0e
alemi <me@alemi.dev>
2025-11-08 15:50:00 +0100

    feat: all debug actions command now work

--- src/main/java/ftbsc/tspr/commands/DebugActions.java
+++ src/main/java/ftbsc/tspr/commands/DebugActions.java
@@ -5,10 +5,12 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import ftbsc.tspr.api.ILoadable;
 import ftbsc.tspr.api.command.BaseCommand;
-import net.minecraft.client.gui.components.AbstractWidget;
+import ftbsc.tspr.helpers.Chat;
+import net.minecraft.client.Options;
 import net.minecraft.client.gui.screens.debug.GameModeSwitcherScreen;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.util.Mth;
 import net.neoforged.neoforge.server.command.EnumArgument;
 
 @AutoService(ILoadable.class)
@@ -25,6 +27,7 @@ public class DebugActions extends BaseCommand {
 				Commands.literal("reloadchunks")
 					.executes(ctx -> {
 						MC.levelRenderer.allChanged();
+						Chat.message("reloading chunks");
 						return 0;
 					})
 			)
@@ -33,6 +36,7 @@ public class DebugActions extends BaseCommand {
 					.executes(ctx -> {
 						boolean flag = !MC.getEntityRenderDispatcher().shouldRenderHitBoxes();
 						MC.getEntityRenderDispatcher().setRenderHitBoxes(flag);
+						Chat.message("entity hitboxes %s", flag ? "enabled" : "disabled");
 						return 0;
 					})
 			)
@@ -41,34 +45,36 @@ public class DebugActions extends BaseCommand {
 					.executes(ctx -> {
 						if (MC.gui != null) {
 							MC.gui.getChat().clearMessages(false);
+							Chat.message("cleared chat messages");
 							return 0;
 						}
 						return 1;
 					})
 			)
-			// .then(
-			// 	Commands.literal("renderdistance")
-			// 		.then(
-			// 			Commands.argument("action", EnumArgument.enumArgument(RenderDistanceAction.class))
-			// 				.executes(ctx -> {
-			// 					RenderDistanceAction action = ctx.getArgument("action", RenderDistanceAction.class);
-			// 					double new_distance = MathHelper.clamp(
-			// 						(double)(MC.options.renderDistance + (action == RenderDistanceAction.INCREASE ? +1 : -1)),
-			// 						AbstractWidget.RENDER_DISTANCE.getMinValue(), AbstractOption.RENDER_DISTANCE.getMaxValue()
-			// 					);
-			// 					AbstractOption.RENDER_DISTANCE.set(MC.options, new_distance);
-			// 					log("debug.cycle_renderdistance.message");
-			// 					return 0;
-			// 				})
-			// 		)
-			// 		.executes(ctx -> {
-			// 			return 1;
-			// 		})
-			// )
+			.then(
+				Commands.literal("renderdistance")
+					.then(
+						Commands.argument("action", EnumArgument.enumArgument(RenderDistanceAction.class))
+							.executes(ctx -> {
+								RenderDistanceAction action = ctx.getArgument("action", RenderDistanceAction.class);
+								int new_distance = Mth.clamp(
+									MC.options.renderDistance().get() + (action == RenderDistanceAction.INCREASE ? +1 : -1),
+									Options.RENDER_DISTANCE_TINY, Options.RENDER_DISTANCE_REALLY_FAR
+								);
+								MC.options.renderDistance().set(new_distance);
+								Chat.message("set render distance to %d", new_distance);
+								return 0;
+							})
+					)
+					.executes(ctx -> {
+						return 1;
+					})
+			)
 			.then(
 				Commands.literal("boundaries")
 					.executes(ctx -> {
 						boolean flag1 = MC.debugRenderer.switchRenderChunkborder();
+						Chat.message("chunk boundaries %s", flag1 ? "enabled" : "disabled");
 						return 0;
 					})
 			)
@@ -87,13 +93,13 @@ public class DebugActions extends BaseCommand {
 						return 0;
 					})
 			)
-			// .then(
-			// 	Commands.literal("gamemode")
-			// 		.executes(ctx -> {
-			// 			MC.setScreen(new GamemodeSelectionScreen());
-			// 			return 0;
-			// 		})
-			// )
+			.then(
+				Commands.literal("gamemode")
+					.executes(ctx -> {
+						MC.setScreen(new GameModeSwitcherScreen());
+						return 0;
+					})
+			)
 			.executes(ctx -> {
 				return 1;
 			});
